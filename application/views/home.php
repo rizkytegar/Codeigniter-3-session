0f82369894d1e55ff6d690b2c9d86f2205cb885f
rizkytegar <mail.rizkytegar@gmail.com>
2021-04-27 23:26:07 +0700

    upload

---
+++ application/views/home.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+ 	<link rel="stylesheet" href="<?php echo base_url("assets/css/bootstrap.min.css")?>">
+    <title>Hello, world!</title>
+  </head>
+  <body>
+	<div class="container py-5">
+		<div class="bg-dark text-secondary px-4 py-5 text-center">
+			<div class="py-5">
+				<h1 class="display-5 fw-bold text-white">RizkyDev</h1>
+				<div class="col-lg-6 mx-auto">
+				<p class="fs-5 mb-4">Codeigniter 3 and Bootstrap 5</p>
+				<div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
+					<a href="https://github.com/rizkytegar" type="button" class="btn btn-outline-info btn-lg px-4 me-sm-3 fw-bold">Github</a>
+				</div>
+				</div>
+			</div>
+		</div>
+	</div>
+  <script src="<?php echo base_url("assets/js/bootstrap.bundle.js")?>"></script>
+  </body>
+</html>
---
+++ readme.rst
@@ -0,0 +1,5 @@
+###################
+Codeigniter 3 and Bootstrap 5
+###################
+
+- github.com/rizkytegar
